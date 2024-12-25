58ea18757c173b37b2042cb52916296b1259faa1
Utkarsh Raj <137909876+utkarshraj2004@users.noreply.github.com>
2024-12-25 18:30:56 +0000

    Learned Variables In Js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 131434
+let accountEmail = "sfsf@hotmail.com"
+var accountPassword = "2434"
+accountCity = "Lucknow"
+let accountState;
+
+// accountId = 2 //Not Allowed
+accountEmail = "hc@gmail.com"
+accountPassword = "Bengaluru"
+accountCity = "Delhi"
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
