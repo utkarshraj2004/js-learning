f000fcbedbf974187e63c531eb0e95e037e62a52
Utkarsh Raj <137909876+utkarshraj2004@users.noreply.github.com>
2024-12-25 18:56:54 +0000

    Datatypes in Javascript

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //Treat all the JS code as newer version
+
+// alert(3+3) //we are using nodejs, not browser.
+
+console.log(3 + 3);
+// Code readability that is high.
+
+console.log("Utkarsh");
+
+
+let name = "Utkarsh"
+let age = 18
+let isLogged = false
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => uniquness
+
+console.log(typeof undefined);
+console.log(typeof null);
