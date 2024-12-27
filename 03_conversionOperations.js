a8ce0f543745792ad650da808b151ae17651b62f
Utkarsh Raj <137909876+utkarshraj2004@users.noreply.github.com>
2024-12-27 16:19:29 +0000

    Coversion Of Datatypes

---
+++ 03_conversionOperations.js
@@ -0,0 +1,24 @@
+let score = true
+
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => Nan
+// true => 1 , false => 0
+
+let isLoggedIn = "utkarsh"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0=> false
+// " => false
+// // "utkarsh" => true
+
+let Numner = 33
+let stringNumber = String(Numner)
+console.log(stringNumber);
+console.log(typeof(stringNumber));
